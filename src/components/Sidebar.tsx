4d4629a8d396749f008464249d035f95bfb60bc3
Ayush Trehan <ayush.trehan100@gmail.com>
2024-05-23 18:53:40 +0530

    add react icons package and update message bubble

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,12 @@
+import { BiMessageRoundedDetail } from "react-icons/bi";
+
 const Sidebar = () => {
   return (
     <div className="basis-1/3 border-l-2 border-gray-100 p-3">
-      <button className="flex items-center justify-center rounded border-2 border-blue-900/60 px-10 py-2 font-semibold text-blue-900/70">
+      <span className="flex max-w-max flex-col items-center justify-center gap-1 rounded border-2 border-blue-900/60 px-12 py-2 font-semibold text-blue-900/70">
+        <BiMessageRoundedDetail size={30} />
         Message
-      </button>
+      </span>
     </div>
   );
 };
