a4c537db649ae28aa331de65c58e492302b0afe2
Ayush Trehan <ayush.trehan100@gmail.com>
2024-05-23 19:02:36 +0530

    update tailwind config file

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -5,7 +5,7 @@ const BaseLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <div className="flex flex-1">
+      <div className="flex flex-1 flex-row">
         <main className="basis-2/3 p-4">{children}</main>
         <Sidebar />
       </div>
