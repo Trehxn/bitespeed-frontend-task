8d44255f0ea932005f3c8ab853d5e2c8b9b2a188
Ayush Trehan <ayush.trehan100@gmail.com>
2024-05-23 18:40:11 +0530

    create a reusable Sidebar component

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+const Sidebar = () => {
+  return (
+    <div className="basis-1/3 border-l-2 border-gray-100 p-3">
+      <button className="flex items-center justify-center rounded border-2 border-blue-900/60 px-10 py-2 font-semibold text-blue-900/70">
+        Message
+      </button>
+    </div>
+  );
+};
+
+export default Sidebar;
