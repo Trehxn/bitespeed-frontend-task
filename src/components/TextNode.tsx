f0dee419fc55fc3edcf3d09c95c5bfbdbd185dd1
Ayush Trehan <ayush.trehan100@gmail.com>
2024-05-24 19:31:12 +0530

    update text node styling and add props support

--- src/components/TextNode.tsx
+++ src/components/TextNode.tsx
@@ -1,22 +1,24 @@
-import { Handle, Position } from "reactflow";
+import { Position, NodeProps } from "reactflow";
 import { BiMessageRoundedDetail } from "react-icons/bi";
 import { FaWhatsapp } from "react-icons/fa";
 
-const TextNode = () => {
+import NodeHandle from "@/components/NodeHandle";
+
+const TextNode: React.FC<NodeProps> = ({ data }) => {
   return (
-    <div className="min-w-80 overflow-hidden rounded-lg shadow-2xl">
-      <Handle type="source" position={Position.Left} />
+    <div className="custom-shadow min-w-80 overflow-hidden rounded-lg bg-white">
+      <NodeHandle type="target" position={Position.Left} />
       <div className="flex items-center justify-center bg-teal-200 px-4 py-1">
-        <BiMessageRoundedDetail size={15} />
+        <BiMessageRoundedDetail size={15} color="#63928A" />
         <span className="ml-1 flex-1 font-bold">Send Message</span>
         <div className="flex h-4 w-4 items-center justify-center rounded-full bg-white">
           <FaWhatsapp size={12} color="#25D366" />
         </div>
       </div>
-      <div className="px-4 py-3 font-semibold text-gray-700">
-        test message 1
+      <div className="mb-1 px-4 py-3 font-semibold text-gray-700">
+        {data.label}
       </div>
-      <Handle type="target" position={Position.Right} />
+      <NodeHandle type="source" position={Position.Right} />
     </div>
   );
 };
