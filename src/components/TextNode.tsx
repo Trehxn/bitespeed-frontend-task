1990862a34edd96ed31eb2abcd8e68e1e6266d54
Ayush Trehan <ayush.trehan100@gmail.com>
2024-05-27 16:39:22 +0530

    update whatsapp logo

--- src/components/TextNode.tsx
+++ src/components/TextNode.tsx
@@ -1,8 +1,9 @@
+import Image from "next/image";
 import { Position, NodeProps } from "reactflow";
 import { BiMessageRoundedDetail } from "react-icons/bi";
-import { FaWhatsapp } from "react-icons/fa";
 
 import NodeHandle from "@/components/NodeHandle";
+import whatsappImage from "../../public/whatsapp-logo.png";
 
 const TextNode: React.FC<NodeProps> = ({ data, selected }) => {
   return (
@@ -14,7 +15,12 @@ const TextNode: React.FC<NodeProps> = ({ data, selected }) => {
         <BiMessageRoundedDetail size={15} color="#63928A" />
         <span className="ml-1 flex-1 font-bold">Send Message</span>
         <div className="flex h-4 w-4 items-center justify-center rounded-full bg-white">
-          <FaWhatsapp size={12} color="#25D366" />
+          <Image
+            src={whatsappImage}
+            alt="whatsapp logo"
+            width={10}
+            height={10}
+          />
         </div>
       </div>
       <div className="mb-1 px-4 py-3 font-semibold text-gray-600">
