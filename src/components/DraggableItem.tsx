64ce38f53927b8df728589adde7ed66955f8071b
Ayush Trehan <ayush.trehan100@gmail.com>
2024-05-27 17:08:36 +0530

    refactor draggable item logic into a separate component

---
+++ src/components/DraggableItem.tsx
@@ -0,0 +1,28 @@
+type DraggableItemProps = {
+  label: string;
+  nodeType: "textNode" | "";
+  message: string;
+  icon?: React.ReactElement;
+};
+
+// reusable component that renders a draggable element
+const DraggableItem: React.FC<DraggableItemProps> = ({
+  icon,
+  nodeType,
+  message,
+  label,
+}) => (
+  <div
+    className="flex min-w-44 max-w-max flex-col items-center justify-center gap-1 rounded border-2 border-blue-900/60 px-12 py-2 font-semibold text-blue-900/70 hover:cursor-grab active:cursor-grabbing"
+    draggable
+    onDragStart={(e) => {
+      e.dataTransfer.setData("nodeType", nodeType);
+      e.dataTransfer.setData("message", message);
+    }}
+  >
+    {icon}
+    {label}
+  </div>
+);
+
+export default DraggableItem;
