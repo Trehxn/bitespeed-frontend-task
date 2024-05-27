0500fb3f77289b0161e2767fa2d82d426d8aef83
Ayush Trehan <ayush.trehan100@gmail.com>
2024-05-27 15:52:13 +0530

    add support for updating styles through props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,9 @@
 type Props = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button: React.FC<Props> = ({ children, ...rest }) => {
+const Button: React.FC<Props> = ({ children, className, ...rest }) => {
   return (
     <button
-      className="flex items-center justify-center rounded-lg border-2 border-blue-900 bg-white px-8 py-2 font-bold text-blue-900 hover:bg-blue-900 hover:text-white active:border-blue-700 active:bg-blue-700"
+      className={`flex items-center justify-center rounded-lg border-2 border-blue-900 bg-white px-8 py-2 font-bold text-blue-900 hover:bg-blue-900 hover:text-white active:border-blue-700 active:bg-blue-700 ${className}`}
       {...rest}
     >
       {children}
